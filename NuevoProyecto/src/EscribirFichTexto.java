16d02e0a4571ea69b94694e9e2489d792f5cebcc
JokinU-96 <jokin.urteaga@ikasle.egibide.org>
2025-09-10 20:43:33 +0200

    Actividad 4 con write(int c)

--- NuevoProyecto/src/EscribirFichTexto.java
+++ NuevoProyecto/src/EscribirFichTexto.java
@@ -6,12 +6,17 @@ public class EscribirFichTexto {
     public static void main(String[] args) {
         File fichero = new File("FichTexto.txt");
         try {
-            FileWriter fw = new FileWriter(fichero, true);
+            FileWriter fw = new FileWriter(fichero, false);
             String texto = "El texto que deseo escribir.";
+            String textoB = "\nEl texto que escribo con un bucle For";
 
             char[] ta = texto.toCharArray();
 
             fw.write(ta);
+
+            for(int i = 0; i < textoB.length(); i++) {
+                fw.write(textoB.charAt(i));
+            }
 
             fw.close();
 
