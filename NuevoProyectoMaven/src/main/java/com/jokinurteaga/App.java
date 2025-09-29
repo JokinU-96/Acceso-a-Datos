a6a3e32033a975a12a3ef56b56618fe7d210e92b
JokinU-96 <jokin.urtaga@ikasle.egibide.org>
2025-09-29 21:26:15 +0200

    Baes de Datos

---
+++ NuevoProyectoMaven/src/main/java/com/jokinurteaga/App.java
@@ -0,0 +1,23 @@
+package com.jokinurteaga;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        BaseDeDatos bd = new BaseDeDatos();
+        ArrayList<Empleado> empleados = bd.getEmpleadosByDeptNo(10);
+
+        System.out.println("Empleados del departamento 10: ");
+        for (Empleado empleado : empleados) {
+            System.out.println(empleado);
+        }
+
+
+    }
+}
